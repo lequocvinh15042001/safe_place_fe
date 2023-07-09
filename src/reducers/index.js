70f333472638bad27d13a2bc8e64862dd560cd6f
lequocvinh <lequocvinh2001@gmail.com>
2023-07-09 09:45:36 +0200

    setup react ui

---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from "redux";
+// import authReducer from "./Auth";
+// import movieReducer from "./Movie";
+// import usersManagementReducer from "./UsersManagement";
+// import eventsManagementReducer from "./EventsManagement";
+// import reviewsManagementReducer from "./ReviewsManagement";
+// import theaterReducer from "./Theater";
+// import bookTicketReducer from "./BookTicket";
+// import movieDetailReducer from "./MovieDetail";
+// import modalTrailerReducer from "./ModalTrailer";
+// import branchManagementReducer from "./Branch";
+// import ticketReducer from "./Ticket";
+// import lazyReducer from "./Lazy";
+// import categoryManagementReducer from "./Category";
+// import interactionReducer from "./Interaction";
+// import billsManagementReducer from "./Bill";
+
+const rootReducer = combineReducers({
+//   authReducer, movieReducer, usersManagementReducer,
+//   theaterReducer, bookTicketReducer, movieDetailReducer,
+//   modalTrailerReducer, lazyReducer, ticketReducer, eventsManagementReducer, 
+//   reviewsManagementReducer, billsManagementReducer, categoryManagementReducer,
+//   branchManagementReducer, interactionReducer
+});
+export default rootReducer;
